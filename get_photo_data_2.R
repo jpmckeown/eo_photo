5a7d34f95b6c044a9a9f24cc415f61f08bc93bc7
jpmckeown <jpmckeown@users.noreply.github.com>
2021-12-23 13:56:47 +0000

    fix mismatch ID old new for Fiji and Mauritius

--- get_photo_data_2.R
+++ get_photo_data_2.R
@@ -1,12 +1,14 @@
 # 2nd step, relies on df1 created by get_photo_data_1
 
 # get Wikimedia FileURL from early run API data
-load('data/imgdata.Rda')
+load('data/imgdata.rda')
 
-# retrospectively fix ID in imgdata to match Photos doc
-# imgdata[imgdata$iso2c=='FJ' && imgdata$ID==1,]
-imgdata$ID[114] <- 2  
-imgdata$ID[115] <- 3  
+# retrospectively fix IDs in imgdata to match Photos doc
+# imgdata$ID[114] <- 2  # Fiji
+# imgdata$ID[115] <- 3  
+# imgdata$ID[214] <- 2  # Mauritius
+# imgdata$ID[215] <- 3  
+# save(imgdata, file='data/imgdata.rda')
 
 # remember that in imgdata Namibia iso2c is 'NB'
 # imgdata[imgdata$Country=='Namibia',]$iso2c
@@ -30,25 +32,28 @@ df2_earlyFileURL <- left_join(df2, imgdata_earlyFileURL)
 # check that FileURL == EarlyFileURL where both present
 check <- df2_earlyFileURL$FileURL == df2_earlyFileURL$EarlyFileURL
 which(check == FALSE)
-# 198 199 267 268 354  # was not expecting any, good did check!
+# 198 199 267 268 354  # was not expecting any, good I did check!
+# after Fiji and Iran fixed, only 355
+# after Mauritius fixed, none
+#  but worrying as MR 1&2 both ID shifted but only id2 flagged
 
 # can get Attribution HTML now all the earlier FileURL ready
-imgdata <- imgdata %>% 
-  select(iso3c, ID, Info_address, File_address, Attribution)
-names(imgdata) <- c('iso3c', 'ID', 'InfoURL', 'FileURL', 'Attribution')
-
-# mresult <- merge(x=df1, y=imgdata, by=c('iso3c'='iso3c', 'ID'='ID', 'InfoURL'='Info_address', 'FileURL'='File_address'), all.x=TRUE)
-jresult <- left_join(df1, imgdata)
-
-loopEnd <- 1 # nrow(imgdata)
-
-for (oldRun  in 1:loopEnd) {
-  
-  iso_2c <- imgdata$iso2c[oldRun]
-  photo_id <- imgdata$ID[oldRun]
-  
-  row <- df[df$iso2c==iso_2c,]
-  print(row)
+# imgdata <- imgdata %>% 
+#   select(iso3c, ID, Info_address, File_address, Attribution)
+# names(imgdata) <- c('iso3c', 'ID', 'InfoURL', 'FileURL', 'Attribution')
+# 
+# # mresult <- merge(x=df1, y=imgdata, by=c('iso3c'='iso3c', 'ID'='ID', 'InfoURL'='Info_address', 'FileURL'='File_address'), all.x=TRUE)
+# jresult <- left_join(df1, imgdata)
+# 
+# loopEnd <- 1 # nrow(imgdata)
+# 
+# for (oldRun  in 1:loopEnd) {
+#   
+#   iso_2c <- imgdata$iso2c[oldRun]
+#   photo_id <- imgdata$ID[oldRun]
+#   
+#   row <- df[df$iso2c==iso_2c,]
+#   print(row)
 
   # fileURL <- as.character(df2[i, 'FileURL'])
   # infoURL <- as.character(df2[i, 'InfoURL'])
@@ -62,7 +67,7 @@ for (oldRun  in 1:loopEnd) {
   #   df2[i, 'ImageName'] <- imgName
   #   df2[i, 'InfoURL'] <- infoURL
   # }
-}
+#}
 
-saveRDS(df2, 'data/df2.rds')
+#saveRDS(df2, 'data/df2.rds')
 #write_tsv(df2, 'data/photo_step_2.tsv')
