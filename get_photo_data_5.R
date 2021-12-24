d9a2d34e289d56e99fbf4105c793b7a23a532b88
jpmckeown <jpmckeown@users.noreply.github.com>
2021-12-24 11:24:11 +0000

    step 5 adjusted to check if step 2 already provided ArtistHTML

--- get_photo_data_5.R
+++ get_photo_data_5.R
@@ -14,12 +14,10 @@ pixabay_ID_to_df <- function(imgName) {
 
 # retreated to old way using ArtistLine as a whole, instead of finding Artist & URL
 #  because of problems trying to extract them. 
-df5['ArtistHTML'] <- as.character(NA)
+# df5['ArtistHTML'] <- as.character(NA) # already included by Step 2
 
-
-df5 <- df5[, c('Country', 'iso3c', 'ID', 'Caption', 'Provider', 'Artist', 'ArtistURL', 'ArtistHTML', 'License', 'LicenseURL', 'ImageName', 'InfoURL', 'FileURL', 'OriginURL', 'folder', 'CreditHTML', 'Format', 'iso2c', 'w640_URL')]
-# eliminate columns width & height
-# df5 <- df5[, c('Country', 'iso3c', 'ID', 'Caption', 'Provider', 'Artist', 'ArtistURL', 'ArtistHTML', 'License', 'LicenseURL', 'ImageName', 'InfoURL', 'OriginURL', 'FileURL', 'folder', 'CreditHTML', 'Format', 'Width', 'Height', 'iso2c', 'w640_URL')]
+# no new column for this step
+# df5 <- df5[, c('Country', 'iso3c', 'ID', 'Caption', 'Provider', 'Artist', 'ArtistURL', 'ArtistHTML', 'License', 'LicenseURL', 'ImageName', 'InfoURL', 'FileURL', 'folder', 'iso2c', 'OriginURL', 'w640_URL', 'Attribution')]
 
 artistLine_vector <- rep(NA, nrow(df5))
 license_vector <- rep(NA, nrow(df5))
@@ -34,78 +32,84 @@ for (i in 1:loopEnd) {
   # incr(i)
   imgName <- df5$ImageName[i]
   
-  if (df5$Provider[i] == 'Wikimedia' && !is.na(imgName)) {
+  # check artistHTML empty; Step 2 may have supplied already.
+  
+  if (df5$Provider[i] == 'Wikimedia' && !is.na(imgName) ) {
     
-    artist <- NA
-    artistLine <- NA
-    
-    incr(changed)
-    
-    # get Artist name from wikimedia API
-    artist_API <- paste0(
-      'https://commons.wikimedia.org/w/api.php?action=query&titles=File:', 
-      imgName, 
-      '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=Artist&format=json')
-    
-    artist_JSON <- jsonlite::fromJSON(artist_API)
-    artist_here <- unlist(artist_JSON)
-    artistLine1 <- artist_here[ grepl('Artist.value', names(artist_here)) ]
-    artistLine2 <- unname(artistLine1)
-    
-    # harmonise to contain single quotes not double quotes
-    #  but what if doublequotes ArtistLine also contains a singlequote?
-    artistLine3 <- gsub('\"', "'", artistLine2)
-    
-    # avoid \n errors when uploading to spreadsheet
-    artistLine <- gsub('\n', '', artistLine3) 
-
-    artistLine_vector[i] <- artistLine
-    df5$ArtistHTML[i] <- artistLine
-    
-    print(paste(i, df5$iso3c[i], df5$ID[i]))
-    print(paste('artistLine', artistLine))  
-
-    # get license from wikimedia API
-    license_API <- paste0(
-      'https://commons.wikimedia.org/w/api.php?action=query&titles=File:',
-      imgName,
-      '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=LicenseShortName&format=json')
-
-    license_JSON <- jsonlite::fromJSON(license_API)
-    license_here <- unlist(license_JSON)
-    license_line1 <- license_here[ grepl('LicenseShortName.value', names(license_here)) ]
-    license_line2 <- unname(license_line1)
-    
-    # is this needed?
-    # license_line3 <- gsub('\"', "'", license_line2)
-    # avoid \n errors when uploading to spreadsheet
-    license_line <- gsub('\n', '', license_line2)
-    
-    licens <- gsub('.*"(/w*)"', '\\1', license_line)
-    df5$License[i] <- licens
-    license_vector[i] <- licens
-
-    # get URL describing license from wikimedia API
-    licenseURL_API <- paste0(
-      'https://commons.wikimedia.org/w/api.php?action=query&titles=File:',
-      imgName,
-      '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=LicenseUrl&format=json')
-
-    licenseURL_JSON <- jsonlite::fromJSON(licenseURL_API)
-    licenseURL_here <- unlist(licenseURL_JSON)
-    licenseURL_line1 <- licenseURL_here[ grepl('LicenseUrl.value', names(licenseURL_here)) ]
-    licenseURL_line2 <- unname(licenseURL_line1)
-    
-    if (!identical(licenseURL_line2, character(0))) {
+    if (!is.na(df5$artistHTML[i])) {
+      print(paste(i, 'got ArtistHTML from Step 2'))
+    } else {
+      artist <- NA
+      artistLine <- NA
+      
+      incr(changed)
+      
+      # get Artist name from wikimedia API
+      artist_API <- paste0(
+        'https://commons.wikimedia.org/w/api.php?action=query&titles=File:', 
+        imgName, 
+        '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=Artist&format=json')
+      
+      artist_JSON <- jsonlite::fromJSON(artist_API)
+      artist_here <- unlist(artist_JSON)
+      artistLine1 <- artist_here[ grepl('Artist.value', names(artist_here)) ]
+      artistLine2 <- unname(artistLine1)
+      
+      # harmonise to contain single quotes not double quotes
+      #  but what if doublequotes ArtistLine also contains a singlequote?
+      artistLine3 <- gsub('\"', "'", artistLine2)
+      
+      # avoid \n errors when uploading to spreadsheet
+      artistLine <- gsub('\n', '', artistLine3) 
+      
+      artistLine_vector[i] <- artistLine
+      df5$ArtistHTML[i] <- artistLine
+      
+      print(paste(i, df5$iso3c[i], df5$ID[i]))
+      print(paste('artistLine', artistLine))  
+      
+      # get license from wikimedia API
+      license_API <- paste0(
+        'https://commons.wikimedia.org/w/api.php?action=query&titles=File:',
+        imgName,
+        '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=LicenseShortName&format=json')
+      
+      license_JSON <- jsonlite::fromJSON(license_API)
+      license_here <- unlist(license_JSON)
+      license_line1 <- license_here[ grepl('LicenseShortName.value', names(license_here)) ]
+      license_line2 <- unname(license_line1)
+      
+      # is this needed?
+      # license_line3 <- gsub('\"', "'", license_line2)
+      # avoid \n errors when uploading to spreadsheet
+      license_line <- gsub('\n', '', license_line2)
+      
+      licens <- gsub('.*"(/w*)"', '\\1', license_line)
+      df5$License[i] <- licens
+      license_vector[i] <- licens
+      
+      # get URL describing license from wikimedia API
+      licenseURL_API <- paste0(
+        'https://commons.wikimedia.org/w/api.php?action=query&titles=File:',
+        imgName,
+        '&prop=imageinfo&iiprop=extmetadata&iiextmetadatafilter=LicenseUrl&format=json')
+      
+      licenseURL_JSON <- jsonlite::fromJSON(licenseURL_API)
+      licenseURL_here <- unlist(licenseURL_JSON)
+      licenseURL_line1 <- licenseURL_here[ grepl('LicenseUrl.value', names(licenseURL_here)) ]
       licenseURL_line2 <- unname(licenseURL_line1)
-      licenseURL_line <- gsub('\n', '', licenseURL_line2)
-      df5$LicenseURL[i] <- licenseURL_line
-      licenseURL_vector[i] <- licenseURL_line
-      print(paste('licenseURL', licenseURL_line)) 
-    }
+      
+      if (!identical(licenseURL_line2, character(0))) {
+        licenseURL_line2 <- unname(licenseURL_line1)
+        licenseURL_line <- gsub('\n', '', licenseURL_line2)
+        df5$LicenseURL[i] <- licenseURL_line
+        licenseURL_vector[i] <- licenseURL_line
+        print(paste('licenseURL', licenseURL_line)) 
+      }
+      
+      print(paste('license', licens))
 
-    print(paste('license', licens))
-    
+    } # end not previously supplied
   } # end Wikimedia with ImageName
   
   # if API other Providers can get Artist or License, it goes here
