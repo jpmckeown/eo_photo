cc8cc3765431480c3e782f2f09f5d3b9c856b353
jpmckeown <jpmckeown@users.noreply.github.com>
2021-12-27 10:51:39 +0000

    making df6 with two extra columns

--- get_photo_data_6.R
+++ get_photo_data_6.R
@@ -21,11 +21,11 @@ artisthtml_with_2urls <- function(html) {
 # due to unknown state of df 5 after messup while ad hoc fixing 6 missing in step5; may need to retrieve archived df5.rds from other folder and then run step 5 fix on it again
 # df6 <- readRDS('data/df5.rds')
 
-artist_vector <- rep(NA, nrow(df6))
+artist_vector <- df5$Artist # rep(NA, nrow(df6))
 artistURL_vector <- rep(NA, nrow(df6))
 link2nd_vector <- rep(NA, nrow(df6))
 artExtra_vector <- rep(NA, nrow(df6))
-link0=0; link1=0; link2=0
+link0 <- 0; link1 <- 0; link2 <- 0
 
 loopEnd <- nrow(df6)
 for (i in 1:loopEnd) {
@@ -37,7 +37,7 @@ for (i in 1:loopEnd) {
   
   if (df6$Provider[i] == 'Wikimedia') {
     
-    # from ArtistHTML extract Artist and ArtistURL especially helps if
+    # from ArtistHTML extracting Artist and ArtistURL because helps if
     # link is Wikimedia flagged as not existing, or detect link invalid
     
     # where artist's URL provided, artistLine has 1 or 2 Anchor href with \" quotes
@@ -53,14 +53,19 @@ for (i in 1:loopEnd) {
         artist <- artist_html
         print(paste(i, artist))
         incr(link0)
-        artist_vector[i] <- artist
+        # 18 artists in df5, maybe manually found
+        if (is.na(artist_vector[i])) {
+          artist_vector[i] <- artist          
+        }
         
       } else if (links == 1) {
         s <- artisthtml_with_1url(artist_html)
         #print(paste(i, 'one', s[2], s[3], s[4]))
         incr(link1)
         artistURL_vector[i] <- s[2]
-        artist_vector[i] <- s[3]
+        if (is.na(artist_vector[i])) {
+          artist_vector[i] <- s[3]
+        }
         artExtra_vector[i] <- s[4]
         if (grepl('page does not exist', artist_html)) {
           artistURL_vector[i] <- NA  
@@ -71,7 +76,9 @@ for (i in 1:loopEnd) {
         #print(paste(i, s[2], s[3], s[4], s[5], s[6]))
         incr(link2)
         artistURL_vector[i] <- s[2]
-        artist_vector[i] <- s[3]
+        if (is.na(artist_vector[i])) {
+          artist_vector[i] <- s[3]
+        }
         link2nd_vector[i] <- s[5]
         artExtra_vector[i] <- s[6]
         # if (grepl('page does not exist', artist_html)) {
@@ -86,55 +93,32 @@ for (i in 1:loopEnd) {
       }
     } # end test if ArtistHTML present
     
-
-    
-
-    # if ( !is.na(artist_html) ) {
-    #   if (grepl('page does not exist', artist_html)) {
-    #     artist <- sub("^<a (.+)>(.+)<\\/a>", "\\2", artist_html)
-    #     print(paste(i, artist))
-    #   }
-    #} # end artist_html
-    
   } # end Wikimedia 
   
-# if link flagged invalid just use Artist name
-# if (grepl('page does not exist', artistLine)) {
-#   artist <- sub('<.*">', '', artistLine)
-#   artist <- sub('</a>', '', artist)
-#   df6$Artist[i] <- artist
-# } else {
-#   df6$ArtistHTML[i] <- artistLine
-# }
+  # "Unsplash photographers appreciate as it provides exposure to their work and encourages them to continue sharing.
+  if (df6$Provider == 'Unsplash') {
+    df6$LicenseURL <- 'https://unsplash.com/license'
+  }
 
+  if (df6$Provider == 'Pixabay') {
+    df6$License <- 'Pixabay License'
+    df6$LicenseURL <- 'https://pixabay.com/service/license/' # longer https://pixabay.com/service/terms/#license
+  }
 
+  if (df6$Provider == 'Pixnio') {
+    df6$License <- 'CC0'
+    df6$LicenseURL <- 'https://pixnio.com/creative-commons-license'
+  }
 
-# get artist_URL and check if valid and isn't missing at Provider
-  
-
-
-# "Unsplash photographers appreciate it as it provides exposure to their work and encourages them to continue sharing.
-  # if (df6$Provider == 'Unsplash') {
-  #   df6$LicenseURL <- 'https://unsplash.com/license'
-  # }
-  # 
-  # if (df6$Provider == 'Pixabay') {
-  #   df6$License <- 'Pixabay License'
-  #   df6$LicenseURL <- 'https://pixabay.com/service/license/' # longer https://pixabay.com/service/terms/#license
-  # }
-  # 
-  # if (df6$Provider == 'Pixnio') {
-  #   df6$License <- 'CC0'
-  #   df6$LicenseURL <- 'https://pixnio.com/creative-commons-license'
-  # }
-  # 
-  # # if you are using content for editorial purposes, you must include the following credit adjacent to the content or in audio/visual production credits: “FreeImages.com/Artist’s Member Name.”
-  # if (df6$Provider == 'FreeImages') {
-  #   df6$License <- 'FreeImages.com'
-  #   df6$LicenseURL <- 'https://www.freeimages.com/license'
-  # }
+  # if you are using content for editorial purposes, you must include the following credit adjacent to the content or in audio/visual production credits: “FreeImages.com/Artist’s Member Name.”
+  if (df6$Provider == 'FreeImages') {
+    df6$License <- 'FreeImages.com'
+    df6$LicenseURL <- 'https://www.freeimages.com/license'
+  }
 
 } # photo loop
+
+# get artist_URL and check if valid and isn't missing at Provider
 
 # find and count empty artist
 emptyArt <- which(stri_isempty(artist_vector)) # 51  78 163 211 241 242
@@ -144,16 +128,21 @@ print(paste(nEmptyArt, '=', emptyArt))
 # how many Wikimedia with 0 1 2 hyperlinks?
 print(paste('Links 0:', link0, '1:', link1, '2:', link2))
 
-saveRDS(df6, 'data/df6.rds')
+df6$Artist <- artist_vector
+df6$ArtistURL <- artistURL_vector
+df6$ArtistExtra <- artExtra_vector
+df6$Artist2ndURL <- link2nd_vector
 
-# Artist
-# 247 says Own work, no name (i did find it before)
+#saveRDS(df6, 'data/df6.rds')
+
+# Artist, fix ad hoc in Step 7
+#D 247 says Own work, no name (i did find it before)
+#D 499 'photo by: '
+#D 19 User:
 # 560 has 2nd URL in place of Artist name birdphotos.com
 # 39,437 includes 2nd URL as well as Artist
 # 230 Copyright c 
 # 343 &copy; 
-# 499 'photo by: '
-# 19 User:
 # some UPPER CASE 104, 381, 463
 # <bdi> 229
 # 2, 82/200, bits should be in Extra 
