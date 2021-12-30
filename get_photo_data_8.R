185c40713422dfb120d0f9b6d0f06b5cbf75f680
jpmckeown <jpmckeown@users.noreply.github.com>
2021-12-30 07:48:59 +0000

    made Pixabay API work by skipping bad one, got ArtistURL

--- get_photo_data_8.R
+++ get_photo_data_8.R
@@ -1,20 +1,109 @@
-# use API to get URL for download
-# No that was done in Step 4, just check if any missing before download
+# 7th step, assemble CreditHTML for all Providers
+#  no API call so can be run across whole df repeatedly
 
-df8 <- readRDS('data/df6.rds')
-missing <- 0
+artist_remove_photoby <- function(artist) {
+  result <- sub('[P|p]hoto by[:]*[ ]*(.*)', '\\1', artist)
+  return(result)
+}
+# May prefix 'Photo by: ' on all credits.
 
-# count/identify missing URL
-for (i in 1:nrow(df8)) {
-  if (is.na(df8$FileURL[i]) && is.na(df8$w640_URL[i])) {
-    incr(missing)
-  }
+artist_remove_user <- function(artist) {
+  result <- sub("[U|u]ser[:]*[ ]*(.*)", '\\1', artist)
+  return(result)
 }
 
-df8_missing <- df8 %>%
-  filter(is.na(FileURL)) %>%
-  filter(is.na(w640_URL)) %>%
-  select(iso3c, ID, Provider, Country)
+df7a <- readRDS('data/df7.rds')
+df8 <- read_tsv('data/EO_photo_providers.tsv')
 
-print(paste('Missing', missing))
-# Wikimedia, Pixabay
+# 59 Rapponi caused API error
+pixabay_ID_to_df <- function(imgName) {
+  Pixabay_API <- 'https://pixabay.com/api/?key=24587231-d8363fed1919782211f48ccc6&'
+  this_API <- paste0(Pixabay_API, 'id=', imgName)
+  this_JSON <- jsonlite::fromJSON(this_API, simplifyVector = TRUE)
+  # returns list, 3rd item is dataframe
+  df <- this_JSON[['hits']]
+  return(df)
+}
+# "user_id":5475750,"user":"Graham-H"  Yes this matches what InfoURL says
+# https://pixabay.com/users/graham-h-5475750/  so ArtistURL can be constructed.
+loopEnd <- nrow(df8)
+# for (i in 94:loopEnd) {
+#   if ( df8$Provider[i] == 'Pixabay' && !is.na(df8$Artist[i]) ) { #  
+#     imgName <- df8$ImageName[i]
+#     
+#     Pixabay_df <- pixabay_ID_to_df(imgName)
+#     artist <- Pixabay_df$user
+#     u_id <- Pixabay_df$user_id
+#     fileurl <- Pixabay_df$webformatURL
+#     artistURL <- paste0('https://pixabay.com/users/', artist, '-', u_id, '/')
+#     df8$Artist[i] <- artist
+#     df8$ArtistURL[i] <- artistURL
+#     #df8$FileURL[i] <- fileurl
+#     #df8$ArtistURL[i] <- paste0('https://pixabay.com/users/', df8$Artist[i], '-', df8$ImageName[i], '/')
+#     #print(paste(i, df8$Artist[i], df8$ImageName[i], df8$ArtistURL[i]))
+#     print(df8$ArtistURL[i])
+#   }
+# }
+
+# i jumbled by Provider re-ordering? # df8[1,]
+# ad hoc fix Wikimedia link errors
+df8$ArtistInfo[247] <- df8$Artist[247] # Own work
+df8$Artist[247] <- 'Loriski' # manually from InfoURL
+
+loopEnd <- nrow(df8)
+for (i in 1:loopEnd) {
+  
+  artist <- df8$Artist[i]
+  
+  # clean up artist
+  if (!is.na(artist)) {
+    if (grepl('hoto by', artist)) {
+      artist <- artist_remove_photoby(artist)      
+    }
+    if (grep(pattern = 'User|user', x = artist)) {
+      artist <- artist_remove_user(artist)
+    }
+  }
+  
+  # if CreditHTML exists we use that 
+  if (!is.na(df8$CreditHTML)) {
+
+    # Artist, ArtistURL, License, LicenseURL, ImageName, InfoURL
+    #  also provider prefix and suffix, or special way of assembling?
+    # https://creativecommons.org/licenses/publicdomain/
+    
+    if (df8$Provider == 'Wikimedia') {
+      
+      # Artist: ; License: ; Image:
+      # always Artist, License, ImageName, InfoURL
+      # optional fields AristURL, ArtistInfo, LicenseURL
+      
+      credit_html <- paste0('<a href="', df8$ArtistURL[i], '">', df8$Artist[i], '</a>', '<a href="', df8$LicenseURL[i], '">', df8$License[i], '</a>')
+    }
+    
+    if (df8$Provider == 'Unsplash') {
+      credit_html <- ''
+    }
+    
+    # Photo by Artist on Pixnio
+    if (df8$Provider == 'Pixnio') {
+      credit_html <- paste0('Photo by <a href="', df8$InfoURL, '">', df8$Artist[i], '</a> on Pixnio <a href="https://pixnio.com/">free images</a> license <a href="', df8$LicenseURL[i], '">', df8$License[i], '</a>')
+    }
+    
+    if (df8$Provider == 'Pixabay') {
+      credit_html <- ''
+    }
+    
+    # if you are using content for editorial purposes, you must include the following credit adjacent to the content: “FreeImages.com/Artist’s Member Name.”
+    if (df8$Provider == 'FreeImages') {
+      credit_html <- paste0('<a href="', df8$LicenseURL[i], '">', df8$License[i], '</a> / Artist: <a href="', df8$ArtistURL[i], '">', df8$Artist[i], '</a>')
+    }
+    
+    df8$CreditHTML[i] <- credit_html    
+  }
+  
+}
+linkLicense <- paste0("<a href='", licenseURL_line2, "'>", licens, "</a>")
+
+
+saveRDS(df8, 'data/df8.rds')
