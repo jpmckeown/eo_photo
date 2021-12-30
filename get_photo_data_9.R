a782ce2bbf0ff0e3f7819452d203466ad263cf61
jpmckeown <jpmckeown@users.noreply.github.com>
2021-12-30 13:33:35 +0000

    test for any values not 0 or 1 length(stored)

--- get_photo_data_9.R
+++ get_photo_data_9.R
@@ -7,7 +7,6 @@ df9['file'] <- as.character(NA)
 missing <- 0
 new_folder <- 'w640/'
 new_photos <- list.files(new_folder)
-#df9$file[i] <- 'Y'
 
 # count/identify missing FileURL
 loopEnd <- nrow(df9)
@@ -22,20 +21,25 @@ for (i in 1:loopEnd) {
   # if image not already stored, try download
   fn <- paste0(iso3c, '_', img_id)
   stored <- grep(fn, new_photos)
-  print(paste(i, fn, stored, length(stored)))
   
   if (length(stored) == 1) {
-    print(paste(i, 'Photo already stored'))
+    print(paste(i, 'Photo already stored', fn, stored, length(stored)))
+    df9$file[i] <- 'Y'
   } 
   else if (length(stored) == 0) {
-    print(paste(i, 'Photo download needed'))
-    
+    print(paste(i, 'Photo download needed', fn, stored, length(stored)))
+    df9$file[i] <- 'N'
     # if (is.na(df8$FileURL[i]) && is.na(df8$w640_URL[i])) {
     #   incr(missing)
     # }
     
   } # end test if stored
 } # end new df loop
+
+# test if any weird stored?
+table(df9$file)
+
+saveRDS(df9, 'data/df9.rds')
 
 # df8_missing <- df8 %>%
 #   filter(is.na(FileURL)) %>%
