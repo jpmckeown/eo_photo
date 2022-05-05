fe6fc746bae62ca21e6e02cac66ebaa1ac8f4544
jpmckeown <j.p.mckeown@icloud.com>
2022-05-05 14:02:47 +0100

    alphabetic sort, new captions copied into df10

--- get_photo_captions.R
+++ get_photo_captions.R
@@ -15,3 +15,26 @@ df10 <- df9 %>%
 which(df10[,1] != df9[,1])
 # many because 3 countries extra ID 3 are at end of list
 
+# sheet imported lacks final column CreditHTML
+# should copy new captions to df10, 
+# in case other columns were processed after import.
+
+import <- photo_import %>% 
+  arrange(Country, ID)
+which(df10[,1] != import[,1])
+which(df10[,3] != import[,3]) # 398 was "3.0"
+
+# data types of columns need checking also
+str(photo_import) # all char
+str(df10) # all chr except ID is numeric
+
+import[398,3] <- "3"
+import$ID <- as.numeric(import$ID)
+ 
+which(df9[,4] != photo_import[,4 ]) # unhelpful
+which(df10[,4] != import[,4 ]) 
+# 7   8  10  25  26  27  35  37 103 125 159 210 211 214 217 220 235 239 243 
+# 248 249 250 283 287 288 291 315 390 398 463 567
+
+df10$Caption <- import$Caption
+saveRDS(df10, '../eo_html/data/df10.rds')
